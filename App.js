a8d5057a96334ae45379421f21d4a6e3cfa733ba
Pemith <pemithr@gmail.com>
2022-02-06 18:11:50 +0530

    Commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image} from 'react-native';
+import { useDimensions,useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
+
+  // console.log(useDimensions());
+  // console.log(useDeviceOrientation());
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Komlo</Text>
       <StatusBar style="auto" />
     </View>
   );
