bcd68aca31f94221ff5266a40c1bf791b09ebf12
Pemith <pemithr@gmail.com>
2022-04-18 22:32:52 +0530

    Login Forms added

--- App.js
+++ App.js
@@ -3,31 +3,27 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Image} from 'react-native';
 import { useDimensions,useDeviceOrientation } from '@react-native-community/hooks';
 
-import WelcomeScreen from './app/Screens/WelcomeScreen';
-import ViewImageScreen from './app/Screens/ViewImageScreen';
 import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/Screens/ListingDetailsScreen';
-import MessageScreen from './app/Screens/MessageScreen';
-import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
 import AccountScreen from './app/Screens/AccountScreen';
-import ListingScreen from './app/Screens/ListingScreen';
 import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
+import Card from './app/components/Card';
+import Icon from './app/components/Icon';
+import ListingDetailsScreen from './app/Screens/ListingDetailsScreen';
+import ListingScreen from './app/Screens/ListingScreen';
+import ListItem from './app/components/ListItem';
+import LoginScreen from './app/Screens/LoginScreen';
+import MessageScreen from './app/Screens/MessageScreen';
+import Screen from './app/components/Screen';
+import ViewImageScreen from './app/Screens/ViewImageScreen';
+import WelcomeScreen from './app/Screens/WelcomeScreen';
 
 
-const categories=[
-  {label:"Furniture", value: 1},
-  {label:"Clothing", value: 2},
-  {label:"Cameras", value: 3},
-];
+
 
 export default function App() {
 
-  const [category,setCategory]=useState()
-
+  
   return (
     // <WelcomeScreen/>
     // <ViewImageScreen/>
@@ -35,17 +31,19 @@ export default function App() {
     // <MessageScreen/>
     // <AccountScreen/>
     // <ListingScreen/>
-    <Screen>
-      {/* <AppTextInput placeholder="Username" icon="email"/> */}
-      <AppPicker 
-        selectedItem={category}
-        onSelectItem={item => setCategory(item)}
-        icon='apps'
-        items={categories} 
-        placeholder="Category" 
-      />
-      <AppTextInput icon='email' placeholder="email"/>
-    </Screen>
+    // <Screen>
+    //   {/* <AppTextInput placeholder="Username" icon="email"/> */}
+    //   <AppPicker 
+    //     selectedItem={category}
+    //     onSelectItem={item => setCategory(item)}
+    //     icon='apps'
+    //     items={categories} 
+    //     placeholder="Category" 
+    //   />
+    //   <AppTextInput icon='email' placeholder="email"/>
+    // </Screen>
+
+    <LoginScreen/>
   );
 }
 
