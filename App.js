36e9789469cabfa3ad28b57ae81f20181fb27378
Pemith <pemithr@gmail.com>
2022-04-29 22:21:50 +0530

    Commit
    AppPicker Edited
    AppTextInput edited
    AppFormPicker Addded
    From folder index file was edited
    ListingEditScreen added

--- App.js
+++ App.js
@@ -1,29 +1,28 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image} from 'react-native';
-import { useDimensions,useDeviceOrientation } from '@react-native-community/hooks';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
-import AppButton from './app/components/AppButton';
-import AccountScreen from './app/Screens/AccountScreen';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
-import Card from './app/components/Card';
-import Icon from './app/components/Icon';
-import ListingDetailsScreen from './app/Screens/ListingDetailsScreen';
-import ListingScreen from './app/Screens/ListingScreen';
-import ListItem from './app/components/ListItem';
-import LoginScreen from './app/Screens/LoginScreen';
-import MessageScreen from './app/Screens/MessageScreen';
-import Screen from './app/components/Screen';
-import ViewImageScreen from './app/Screens/ViewImageScreen';
-import WelcomeScreen from './app/Screens/WelcomeScreen';
-
-
-
+import AppButton from "./app/components/AppButton";
+import AccountScreen from "./app/Screens/AccountScreen";
+import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+import Card from "./app/components/Card";
+import Icon from "./app/components/Icon";
+import ListingDetailsScreen from "./app/Screens/ListingDetailsScreen";
+import ListingScreen from "./app/Screens/ListingScreen";
+import ListItem from "./app/components/ListItem";
+import LoginScreen from "./app/Screens/LoginScreen";
+import MessageScreen from "./app/Screens/MessageScreen";
+import Screen from "./app/components/Screen";
+import ViewImageScreen from "./app/Screens/ViewImageScreen";
+import WelcomeScreen from "./app/Screens/WelcomeScreen";
+import ListingEditScreen from "./app/Screens/ListingEditScreen";
 
 export default function App() {
-
-  
   return (
     // <WelcomeScreen/>
     // <ViewImageScreen/>
@@ -33,18 +32,17 @@ export default function App() {
     // <ListingScreen/>
     // <Screen>
     //   {/* <AppTextInput placeholder="Username" icon="email"/> */}
-    //   <AppPicker 
+    //   <AppPicker
     //     selectedItem={category}
     //     onSelectItem={item => setCategory(item)}
     //     icon='apps'
-    //     items={categories} 
-    //     placeholder="Category" 
+    //     items={categories}
+    //     placeholder="Category"
     //   />
     //   <AppTextInput icon='email' placeholder="email"/>
     // </Screen>
 
-    <LoginScreen/>
+    // <LoginScreen/>
+    <ListingEditScreen />
   );
 }
-
-
